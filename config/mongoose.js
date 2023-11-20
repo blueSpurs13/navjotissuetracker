5856f3696e605e67e96f904e8f159493bbac0494
blueSpurs13 <140317836+blueSpurs13@users.noreply.github.com>
2023-11-20 16:30:35 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+//require the library
+const mongoose = require('mongoose');
+
+// here we are using the MongoDB Url we defined in our ENV file
+let DB_URL = process.env.DB_URL;
+
+//connect to the database
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function () {
+  console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
